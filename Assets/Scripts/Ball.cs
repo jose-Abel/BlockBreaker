35387d44fdeb9d5ca3b68c895d03492e73b7b127
joseabel <joseabel.ramirezfrontany@gmail.com>
2020-06-26 00:15:56 -0400

    Block Breaker Game in C#

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //config params
+    [SerializeField] Paddle paddle1;
+
+    [SerializeField] float xPush = 2f;
+
+    [SerializeField] float yPush = 15f;
+
+    [SerializeField] AudioClip[] ballSounds;
+
+    [SerializeField] float randomFactor = 0.5f;
+
+    // state
+    Vector2 paddleToBallVector;
+
+    bool hasStarted = false;
+
+    // cached component references
+
+    AudioSource myAudioSource;
+
+    Rigidbody2D myRigidBody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+
+        myAudioSource = GetComponent<AudioSource>();
+
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hasStarted != true)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+
+            myRigidBody2D.velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+
+        transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 velocityTweak = new Vector2(Random.Range(0f, randomFactor), Random.Range(0f, randomFactor));
+
+        if(hasStarted)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+
+            myAudioSource.PlayOneShot(clip);
+
+            myRigidBody2D.velocity += velocityTweak;
+        }
+    }
+}
